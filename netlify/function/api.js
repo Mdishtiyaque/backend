31a4f7a6876b7bf95f462589f100feb18f4a0a5e
Md Ishtiyaque <mdishtiyaque16302@gmail.com>
2025-06-25 09:54:00 +0530

    change

---
+++ netlify/function/api.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const serverless=require('serverless-http')
+const router=express.Router();
+
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.get('/my',(req,res)=>{
+    res.send("<h1>'this is change'</h1>")
+})
+
+router.get('/twiter',(req,res)=>{
+     res.send('this is twitter ')
+})
+/*
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+*/
+app.use('/.netlify/function/api',router);
+module.exports.handler=serverless(app);
+
